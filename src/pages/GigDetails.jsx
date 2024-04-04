46423908469260de5875d5609a14dbf5bbd14158
LeanneBoro <leanneboro@gmail.com>
2024-04-04 19:13:15 +0300

    More details lol

--- src/pages/GigDetails.jsx
+++ src/pages/GigDetails.jsx
@@ -30,24 +30,20 @@ export function GigDetails() {
     return (
         <section className='main-details'>
             <section className="gig-details">
-                <article>
-                    {/* <h3><svg width="16" height="16" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" fill="currentFill"><path d="M12.773 14.5H3.227a.692.692 0 0 1-.482-.194.652.652 0 0 1-.2-.468V7.884H.5l7.041-6.212a.694.694 0 0 1 .918 0L15.5 7.884h-2.046v5.954a.652.652 0 0 1-.2.468.692.692 0 0 1-.481.194Zm-4.091-1.323h3.409V6.664L8 3.056 3.91 6.664v6.513h3.408v-3.97h1.364v3.97Z"></path></svg> */}
-                    {/* </h3> */}
-                    <h1>{gig.title}</h1>
-                    <div className="about-user">
-                        <div >
-                        <img className="user-pic" src={gig.owner.imgUrl}></img>     
-                        </div>
-                       
-                        <div>
-                            <h2>{gig.owner.fullname}</h2>
-                            <h3>
-                                <span className='star'><svg width="16" height="15" viewBox="0 0 16 15" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" clipRule="evenodd" d="M16 5.81285C16 5.98299 15.875 6.14367 15.75 6.26654L12.2596 9.61248L13.0865 14.3384C13.0962 14.4045 13.0962 14.4612 13.0962 14.5274C13.0962 14.7732 12.9808 15 12.7019 15C12.5673 15 12.4327 14.9527 12.3173 14.8866L8 12.656L3.68269 14.8866C3.55769 14.9527 3.43269 15 3.29808 15C3.01923 15 2.89423 14.7732 2.89423 14.5274C2.89423 14.4612 2.90385 14.4045 2.91346 14.3384L3.74038 9.61248L0.240385 6.26654C0.125 6.14367 0 5.98299 0 5.81285C0 5.5293 0.298077 5.41588 0.538462 5.37807L5.36539 4.68809L7.52885 0.387524C7.61539 0.207939 7.77885 0 8 0C8.22115 0 8.38462 0.207939 8.47115 0.387524L10.6346 4.68809L15.4615 5.37807C15.6923 5.41588 16 5.5293 16 5.81285Z"></path></svg>
-                                </span>{gig.owner.rate}.0</h3>
-                        </div>
+                <h1>{gig.title}</h1>
+                <div className="about-user">
+                    <div className="user-pic">
+                        <img src={gig.owner.imgUrl}></img>
                     </div>
-                    <img className="folio" src="https://res.cloudinary.com/dippyaafg/image/upload/v1712227355/j6r691lenv3jk6cxtbnq.webp"></img>
-                </article>
+                    <div>
+                        <h2>{gig.owner.fullname}</h2> <h3>
+                            <span className='star'><svg width="16" height="15" viewBox="0 0 16 15" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" clipRule="evenodd" d="M16 5.81285C16 5.98299 15.875 6.14367 15.75 6.26654L12.2596 9.61248L13.0865 14.3384C13.0962 14.4045 13.0962 14.4612 13.0962 14.5274C13.0962 14.7732 12.9808 15 12.7019 15C12.5673 15 12.4327 14.9527 12.3173 14.8866L8 12.656L3.68269 14.8866C3.55769 14.9527 3.43269 15 3.29808 15C3.01923 15 2.89423 14.7732 2.89423 14.5274C2.89423 14.4612 2.90385 14.4045 2.91346 14.3384L3.74038 9.61248L0.240385 6.26654C0.125 6.14367 0 5.98299 0 5.81285C0 5.5293 0.298077 5.41588 0.538462 5.37807L5.36539 4.68809L7.52885 0.387524C7.61539 0.207939 7.77885 0 8 0C8.22115 0 8.38462 0.207939 8.47115 0.387524L10.6346 4.68809L15.4615 5.37807C15.6923 5.41588 16 5.5293 16 5.81285Z"></path></svg>
+                            </span>{gig.owner.rate}.0</h3>
+                    </div>
+
+
+                </div>
+                <img className="folio" src="https://res.cloudinary.com/dippyaafg/image/upload/v1712227355/j6r691lenv3jk6cxtbnq.webp"></img>
 
                 {/* {/* {/* <div className='reviews'>
                     <h1>What people loved about this seller</h1>
@@ -59,7 +55,7 @@ export function GigDetails() {
                 </div>
                 <ReviewList /> */}
             </section>
-            {/* <aside className="sidebar"> */}
+            <aside className="sidebar">
                 <div className="packages-tabs">
                     <h1>basic</h1>
                     <h2>₪{gig.price}</h2>
@@ -77,11 +73,8 @@ export function GigDetails() {
                     <h3><svg width="16" height="16" viewBox="0 0 11 9" xmlns="http://www.w3.org/2000/svg" fill="currentFill"><path d="M3.645 8.102.158 4.615a.536.536 0 0 1 0-.759l.759-.758c.21-.21.549-.21.758 0l2.35 2.349L9.054.416c.21-.21.55-.21.759 0l.758.758c.21.21.21.55 0 .759L4.403 8.102c-.209.21-.549.21-.758 0Z"></path></svg></h3>
                     <button className=''>continue <svg className="arrow" width="16" height="16" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg"><path d="M9.92332 2.96885C9.63854 2.66807 9.1768 2.66807 8.89202 2.96885C8.60723 3.26963 8.60723 3.75729 8.89202 4.05807L11.6958 7.01931H1.48616C1.08341 7.01931 0.756918 7.36413 0.756918 7.7895C0.756918 8.21487 1.08341 8.5597 1.48616 8.5597H11.8436L8.89202 11.677C8.60723 11.9778 8.60723 12.4654 8.89202 12.7662C9.1768 13.067 9.63854 13.067 9.92332 12.7662L14.0459 8.41213C14.3307 8.11135 14.3307 7.62369 14.0459 7.32291L13.977 7.25011C13.9737 7.24661 13.9704 7.24315 13.9671 7.23972L9.92332 2.96885Z"></path></svg></button>
                 </div>
-
-
                 <button className="contact">Contact me</button>
-
-            {/* </aside> */}
+            </aside>
         </section>
     )
 }
