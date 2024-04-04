814c6724108e172353025ee488667d51f4be750c
omerKaduri1 <kaduriomer@gmail.com>
2024-04-04 22:46:48 +0300

    Add style to hero and hero filter

--- src/cmps/Hero.jsx
+++ src/cmps/Hero.jsx
@@ -5,6 +5,9 @@ export function Hero() {
         <div className="home-page-slider">
             <div className="hero-container">
                 <div className="hero-img full">
+                    <div className="hero-title">
+                        <h1 className="title">Find the right <i>freelance</i> <br />service, right away</h1>
+                    </div>
                     <HeroFilter />
                 </div>
             </div>
