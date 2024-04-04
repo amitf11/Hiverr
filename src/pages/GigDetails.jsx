4de5531c43f43bd5f514fb78ee7d64656537bcd8
LeanneBoro <leanneboro@gmail.com>
2024-04-04 12:46:31 +0300

    Commit details

--- src/pages/GigDetails.jsx
+++ src/pages/GigDetails.jsx
@@ -24,26 +24,41 @@ export function GigDetails() {
         }
     }
 
+
+
     if (!gig) return <div>Loading...</div>
     return (
-        <section className="gig-details">
-            <h1>{gig.title}</h1>
-            <div className="about-user">
-                <h2>{gig.owner.fullName}</h2>
-                <h3>{gig.owner.rate}</h3>
-            </div>
-            <div className="img-car">
-                <img src={gig.img} />
-            </div>
-            <div>
-                <h1>What people loved about this seller</h1>
-                <a herf="reviews">See all reviews</a>
-            </div>
-            <div className="top-reviews"></div>
-            <div>
-                {gig.about}
-            </div>
-            <ReviewList/>
+        <section className='main-details'>
+            <section className="gig-details">
+                <article>
+                    <h1>{gig.title}</h1>
+                    <div className="about-user">
+                        <h2>{gig.owner.fullname}</h2>
+                        <h3>{gig.owner.rate}</h3>
+                    </div>
+                    <div className="img-gig">
+                        <img src={gig.img} />
+                    </div>
+
+                </article>
+
+                <div className='reviews'>
+                    <h1>What people loved about this seller</h1>
+                    <a herf="reviews">See all reviews</a>
+                </div>
+                <div className="top-reviews"></div>
+                <div>
+                    {gig.about}
+                </div>
+                <ReviewList />
+            </section>
+            <aside className="sidebar">
+                <div className="packages-tabs">
+                    <h1>basic</h1>
+                    <h2>{gig.price}</h2>
+                    <button>continue</button>
+                </div>
+            </aside>
         </section>
     )
 }
