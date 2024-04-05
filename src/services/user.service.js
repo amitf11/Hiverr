ae548a8bc2b0df3fe117831fb3a6c99b1328ba90
omerKaduri1 <kaduriomer@gmail.com>
2024-04-05 14:59:11 +0300

    Fix and update user service

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -13,18 +13,14 @@ export const userService = {
     getById,
     remove,
     update,
-    changeScore
 }
 
 window.userService = userService
-
 
 function getUsers() {
     return storageService.query('user')
     // return httpService.get(`user`)
 }
-
-
 
 async function getById(userId) {
     const user = await storageService.get('user', userId)
@@ -32,17 +28,22 @@ async function getById(userId) {
     return user
 }
 
+async function getUserReviews(userId) {
+    const user = await storageService.get('user', userId)
+    // const user = await httpService.get(`user/${userId}`)
+    return user.reviews
+}
+
 function remove(userId) {
     return storageService.remove('user', userId)
     // return httpService.delete(`user/${userId}`)
 }
 
-async function update({ _id, score }) {
+async function update({ _id }) {
     const user = await storageService.get('user', _id)
-    user.score = score
     await storageService.put('user', user)
 
-    // const user = await httpService.put(`user/${_id}`, {_id, score})
+    // user = await httpService.put(`user/${user._id}`, user)
 
     // When admin updates other user's details, do not update loggedinUser
     if (getLoggedinUser()._id === user._id) saveLocalUser(user)
@@ -58,7 +59,6 @@ async function login(userCred) {
 
 async function signup(userCred) {
     if (!userCred.imgUrl) userCred.imgUrl = 'https://cdn.pixabay.com/photo/2020/07/01/12/58/icon-5359553_1280.png'
-    userCred.score = 10000
     const user = await storageService.post('user', userCred)
     // const user = await httpService.post('auth/signup', userCred)
     return saveLocalUser(user)
@@ -69,17 +69,16 @@ async function logout() {
     // return await httpService.post('auth/logout')
 }
 
-async function changeScore(by) {
-    const user = getLoggedinUser()
-    if (!user) throw new Error('Not loggedin')
-    user.score = user.score + by || by
-    await update(user)
-    return user.score
-}
-
+// async function changeScore(by) {
+//     const user = getLoggedinUser()
+//     if (!user) throw new Error('Not loggedin')
+//     user.score = user.score + by || by
+//     await update(user)
+//     return user.score
+// }
 
 function saveLocalUser(user) {
-    user = { _id: user._id, fullname: user.fullname, imgUrl: user.imgUrl, score: user.score }
+    user = { _id: user._id, fullname: user.fullname, imgUrl: user.imgUrl }
     sessionStorage.setItem(STORAGE_KEY_LOGGEDIN_USER, JSON.stringify(user))
     return user
 }
