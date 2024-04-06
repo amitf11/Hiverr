2f3f43883b41353b3f0e612ef2f6e7ce89e46488
omerKaduri1 <kaduriomer@gmail.com>
2024-04-06 18:15:23 +0300

    Fix GigIndex layout

--- src/cmps/gig/GigFilter.jsx
+++ src/cmps/gig/GigFilter.jsx
@@ -1,16 +1,18 @@
 export function GigFilter() {
     return (
-        <section>
-            <section className="gig-filter-container flex">
-                <div className="gig-filter">Service options ∨</div>
-                <div className="gig-filter">Seller details ∨</div>
-                <div className="gig-filter">Budget ∨</div>
-                <div className="gig-filter">Delivery time ∨</div>
+        <section className="filter-sort">
+            <section className="gig-filter-container flex space-between">
+                <div className="flex">
+                    <div className="gig-filter">Service options ∨</div>
+                    <div className="gig-filter">Seller details ∨</div>
+                    <div className="gig-filter">Budget ∨</div>
+                    <div className="gig-filter">Delivery time ∨</div>
+                </div>
+                <section className="gig-sort-container flex">
+                    <div className="gig-sort">Sort by: <span className="bold">Best selling ∨</span></div>
+                </section>
             </section>
 
-            <section className="gig-sort-container flex">
-                <div className="gig-sort">Sort by: <span className="bold">Best selling ∨</span></div>
-            </section>
 
         </section>
     )
