de0e559e4faa6418969c0558717e000990c0a5a9
omerKaduri1 <kaduriomer@gmail.com>
2024-04-15 14:10:58 +0300

    Add style

--- src/pages/UserProfile.jsx
+++ src/pages/UserProfile.jsx
@@ -17,6 +17,7 @@ import { UserStatistics } from "../cmps/user-profile/UserStatistics"
 
 import { addGig, loadUserGigs, removeGig } from "../store/actions/gig.actions"
 import { loadOrders, loadSellerOrders } from "../store/actions/order.actions"
+import { UserImg } from "../cmps/UserImg"
 
 export function UserProfile() {
     const [chosenSection, setChosenSection] = useState('orders')
@@ -56,7 +57,7 @@ export function UserProfile() {
     return (
         <section className="flex space-between user-profile">
             <aside className="flex column align-center user-profile-card">
-                <img src={user.imgUrl} />
+                <UserImg imgUrl={user.imgUrl} size={150}/>
                 <h2 className="user-fullname">{user.fullName}</h2>
                 <h4 className="member-since flex space-between">
                     <div>Member since:</div>
