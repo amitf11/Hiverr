9fba25c204bfe3e27376b7c230c70fc4074a8600
omerKaduri1 <kaduriomer@gmail.com>
2024-04-16 11:16:34 +0300

    Fix user message display

--- src/cmps/UserMsg.jsx
+++ src/cmps/UserMsg.jsx
@@ -1,24 +1,33 @@
 import { useEffect, useRef, useState } from "react"
-import { showUserMsg } from "../services/event-bus.service"
+import { SHOW_MSG, eventBus, showUserMsg } from "../services/event-bus.service"
 import { SOCKET_EVENT_ORDER_ADDED, SOCKET_EVENT_ORDER_UPDATED, socketService } from "../services/socket.service"
 
 export function UserMsg() {
-    const [msg, setMsg] = useState('')
+    const [msg, setMsg] = useState(null)
     const timeoutRef = useRef()
 
+    console.log(msg);
+
     useEffect(() => {
-        if (msg) {
-            timeoutRef.current = setTimeout(() => {
-                setMsg(null);
-            }, 4000);
-        }
+        eventBus.on(SHOW_MSG, (msg) => {
+            setMsg(msg)
+            if (timeoutRef.current) {
+                timeoutRef.current = null
+                clearTimeout(timeoutRef.current)
+            }
+            timeoutRef.current = setTimeout(closeMsg, 4000)
+        })
+        // if (msg) {
+        //     timeoutRef.current = setTimeout(() => {
+        //         setMsg(null);
+        //     }, 4000);
+        // }
         // setMsg(msg)
-        // // window.scrollTo({ top: 0, behavior: 'smooth' })
         // if (timeoutRef.current) {
         //     timeoutRef.current = null
         //     clearTimeout(timeoutRef.current)
         // }
-        timeoutRef.current = setTimeout(setMsg(null), 4000)
+        // timeoutRef.current = setTimeout(setMsg(null), 4000)
 
         socketService.on(SOCKET_EVENT_ORDER_ADDED, (userName) => {
             showUserMsg(`You have a New order from ${userName}`)
@@ -29,13 +38,17 @@ export function UserMsg() {
         })
         return () => {
             if (timeoutRef.current) {
-                clearTimeout(timeoutRef.current);
+                clearTimeout(timeoutRef.current)
             }
         }
-    }, [msg])
+    }, [])
+
+    function closeMsg(){
+        setMsg(null)
+    }
 
     if (!msg) return <span></span>
     return <section className="user-msg">
-        {msg.txt}
+        {msg}
     </section>
 }
