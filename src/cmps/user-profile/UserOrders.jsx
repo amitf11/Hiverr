b6a39a8c0b17a118c01cbd191877bcfbb6593822
omerKaduri1 <kaduriomer@gmail.com>
2024-04-16 18:01:57 +0300

    Add colored status on UserOrders

--- src/cmps/user-profile/UserOrders.jsx
+++ src/cmps/user-profile/UserOrders.jsx
@@ -1,4 +1,16 @@
+import Chip from '@mui/material/Chip'
+
 export function UserOrders({ buyerOrders }) {
+
+    function capitalizeWords(str) {
+        return str.replace(/\b\w/g, match => match.toUpperCase())
+    }
+
+    function getStatusColor(status) {
+        if (status === 'pending') return '#ffbe5b'
+        else if (status === 'rejected') return '#c43333'
+        else if (status === 'approved') return '#1dbf73'
+    }
     return (
         <section className="user-orders">
             <h2>My Orders</h2>
@@ -11,7 +23,8 @@ export function UserOrders({ buyerOrders }) {
                         <div>By: {order.seller.fullname}</div>
                         <div className="flex space-between">
                             <div>${order.gig.price}</div>
-                            <div>{order.status}</div>
+                            <Chip label={capitalizeWords(order.status)} 
+                            style={{ backgroundColor: getStatusColor(order.status), color: 'white' }} />
                         </div>
                     </article>
                 ))}
