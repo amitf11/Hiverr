cb661f3d455b9cfd47a0ad73f1c58edfab13ba5f
omerKaduri1 <kaduriomer@gmail.com>
2024-04-17 13:35:18 +0300

    Fix update status button

--- src/cmps/user-profile/UserDashboard.jsx
+++ src/cmps/user-profile/UserDashboard.jsx
@@ -31,10 +31,6 @@ export function UserDashboard({ sellerOrders }) {
         return str.replace(/\b\w/g, match => match.toUpperCase())
     }
 
-    function capitalizeWords(str) {
-        return str.replace(/\b\w/g, match => match.toUpperCase())
-    }
-
     function onChangeStatus(newStatus) {
         if (newStatus === 'Reject') newStatus = 'rejected'
         else newStatus = 'approved'
@@ -55,15 +51,9 @@ export function UserDashboard({ sellerOrders }) {
                 ) : (
                     <div className="order-table flex column">
                         <div className="table-head flex align-center">
-                            {/* <div className="buyer-col">
-                                <h4>Buyer</h4>
-                            </div> */}
                             <div className="gig-col col">
                                 <h4>Gig</h4>
                             </div>
-                            {/* <div className="due-on-col col">
-                                <h4>Order Date</h4>
-                            </div> */}
                             <div className="total-col col">
                                 <h4>Total</h4>
                             </div>
@@ -73,21 +63,11 @@ export function UserDashboard({ sellerOrders }) {
                         </div>
 
                         <div className="orders-container flex column">
-                            {sellerOrders ? sellerOrders.map(order => (
+                            {sellerOrders.map(order => (
                                 <section key={order._id} className="table-row flex align-center space-between">
-                                    {/* <div className=""> */}
-                                    {/* <div className="buyer-col">
-                                        <img src="https://static.vecteezy.com/system/resources/previews/019/879/186/original/user-icon-on-transparent-background-free-png.png" alt="" />
-                                    </div> */}
                                     <div className="flex gig-col">
                                         <span>{order.gig.title}</span>
                                     </div>
-                                    {/* <div className="due-on-col">
-                                        <span> */}
-                                    {/* 07/04/2024 */}
-                                    {/* {order.date} */}
-                                    {/* </span> */}
-                                    {/* </div> */}
                                     <div className="total total-col">
                                         <span>US${order.gig.price}</span>
                                     </div>
@@ -95,19 +75,69 @@ export function UserDashboard({ sellerOrders }) {
                                         <div className="flex align-center status" style={{ backgroundColor: geStatusBgc(order) }}>
                                             <span>{capitalizeWords(order.status)}</span>
                                         </div>
-                                        <StatusModal
-                                            isModalOpen={isModalOpen}
-                                            onChangeStatus={onChangeStatus}
-                                            onCloseModal={onCloseModal} />
-                                        </div>
+                                        {isModalOpen && (
+                                            <StatusModal
+                                                isModalOpen={isModalOpen}
+                                                onChangeStatus={onChangeStatus}
+                                                onCloseModal={onCloseModal}
+                                            />
+                                        )}
                                     </div>
                                 </section>
-                            )) : ''
-                            }
+                            ))}
                         </div>
                     </div>
                 )}
             </section>
         </section>
-    )
+    );
+
+    // return (
+    //     <section className="dashboard flex column align-center justify-center">
+    //         <h2>Manage Your Services</h2>
+    //         <section className="manage-orders flex column">
+    //             {!sellerOrders || !sellerOrders.length ? (
+    //                 <div>No orders yet</div>
+    //             ) : (
+    //                 <div className="order-table flex column">
+    //                     <div className="table-head flex align-center">
+    //                         <div className="gig-col col">
+    //                             <h4>Gig</h4>
+    //                         </div>
+    //                         <div className="total-col col">
+    //                             <h4>Total</h4>
+    //                         </div>
+    //                         <div className="status-col col">
+    //                             <h4>Status</h4>
+    //                         </div>
+    //                     </div>
+
+    //                     <div className="orders-container flex column">
+    //                         {sellerOrders ? sellerOrders.map(order => (
+    //                             <section key={order._id} className="table-row flex align-center space-between">                      
+    //                                 <div className="flex gig-col">
+    //                                     <span>{order.gig.title}</span>
+    //                                 </div>
+    //                                 <div className="total total-col">
+    //                                     <span>US${order.gig.price}</span>
+    //                                 </div>
+    //                                 <div onClick={() => onOpenStatusModal(order)} className="status-container status-col">
+    //                                     <div className="flex align-center status" style={{ backgroundColor: geStatusBgc(order) }}>
+    //                                         <span>{capitalizeWords(order.status)}</span>
+    //                                     </div>
+    //                                     <StatusModal
+    //                                         isModalOpen={isModalOpen}
+    //                                         onChangeStatus={onChangeStatus}
+    //                                         onCloseModal={onCloseModal} />
+    //                                     </div>
+    //                                 </div>
+    //                             </section>
+    //                         )) : ''
+    //                         }
+    //                     </div>
+    //                 </div>
+    //             )}
+    //         </section>
+    //     </section>
+    // )
 }
