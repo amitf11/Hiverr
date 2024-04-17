f3103db0c22957e9ceb94b956fb3ce0b24eb1240
omerKaduri1 <kaduriomer@gmail.com>
2024-04-17 13:26:41 +0300

    Fix dashboard display

--- src/cmps/user-profile/UserDashboard.jsx
+++ src/cmps/user-profile/UserDashboard.jsx
@@ -27,6 +27,10 @@ export function UserDashboard({ sellerOrders }) {
         setIsModalOpen(false)
     }
 
+    function capitalizeWords(str) {
+        return str.replace(/\b\w/g, match => match.toUpperCase())
+    }
+
     function onChangeStatus(newStatus) {
         if (newStatus === 'Reject') newStatus = 'rejected'
         else newStatus = 'approved'
@@ -83,9 +87,9 @@ export function UserDashboard({ sellerOrders }) {
                                     <div className="total total-col">
                                         <span>US${order.gig.price}</span>
                                     </div>
-                                    <div onClick={() => onOpenStatusModal(order)} className="flex status-container status-col">
-                                        <div className="flex align-center justify-center status" style={{ backgroundColor: geStatusBgc(order) }}>
-                                            <span>{order.status}</span>
+                                    <div onClick={() => onOpenStatusModal(order)} className="status-container status-col">
+                                        <div className="flex align-center status" style={{ backgroundColor: geStatusBgc(order) }}>
+                                            <span>{capitalizeWords(order.status)}</span>
                                         </div>
                                         <StatusModal
                                             isModalOpen={isModalOpen}
