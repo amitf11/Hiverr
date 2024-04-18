902f9ad2ccd83c286045a91050749fb16995b08f
LeanneBoro <leanneboro@gmail.com>
2024-04-18 17:15:15 +0300

    Added another skeleton container to preview loader

--- src/assets/styles/cmps/LoadingList.jsx
+++ src/assets/styles/cmps/LoadingList.jsx
@@ -14,10 +14,13 @@ function SkeletonPreview() {
     return (
         <div className='flex column space-between gig-preview skeleton-preview'>
             <Skeleton className='skeleton-img' variant="rectangular" />
-            <div className='flex align-center gig-owner-details'>
-                <div className='owner-details flex align-center space-between'>
+            <div className='flex align-center space-between gig-owner-details'>
+                <div className='flex align-center space-between owner-details '>
                     <Skeleton className='user-image' variant="circular" width={24} height={24} />
                     <Skeleton className='skeleton-text' variant="rectangular" width={100} />
+                </div>
+                <div className='flex align-center level-container'>
+                    <Skeleton className='level' variant="rectangular" width={60} />
                 </div>
             </div>
             <Skeleton className='skeleton-text price second-line' variant="rectangular" />
