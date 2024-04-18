f4f6493cacf64a84b56e582ab0845954e81e48dc
LeanneBoro <leanneboro@gmail.com>
2024-04-18 11:48:30 +0300

    Started skeleton

--- src/cmps/gig/GigList.jsx
+++ src/cmps/gig/GigList.jsx
@@ -1,8 +1,17 @@
 import { GigPreview } from './GigPreview'
+import { Skeleton } from '@mui/material'
+import { Grid } from '@mui/material'
+
 
 export function GigList({ gigs }) {
 
-    if (!gigs) return <div>Loading...</div>
+    if (!gigs || !gigs.length) return (
+        <Grid container item xs={3} spacing={0} direction="column">
+            <Skeleton variant="rectangular"></Skeleton>
+            <Skeleton variant="rectangular"></Skeleton>
+            <Skeleton variant="rectangular"></Skeleton>
+        </Grid>
+    )
     return (
         <section className='content flex'>
             <section className="grid gigs-container">
